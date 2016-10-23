1f880d04f5b8e5218624930935b7babcc652b5ba
mr_bruckner <piotr.m.jurek@gmail.com>
2016-10-23 17:29:41 +0200

    after import from codepen.io

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/piotr_m_jurek/pen/QNqKYd.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,4 @@
+//closes responsive navbar after click
+$('.navbar-collapse ul li a').click(function(){
+  $('.navbar-collapse').collapse('hide');
+});
